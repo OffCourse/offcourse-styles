6eb7d64f21ca28147018f32001f89e2fcb8540f7
Jan Hein Hoogstad <yeehaa@codingthehumanities.com>
2015-06-16 00:15:29 +0200

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  gulp.src('./scss/**/app.scss')
+  gulp.src('./scss/index.scss')
   .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest('./build'));
+  .pipe(gulp.dest('./'));
 
 });
 
